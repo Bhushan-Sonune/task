d925e9363befb5734d3862cc93e33ae3b122819f
Bhushan-Sonune <sonunebhushan2@gmail.com>
2025-06-07 11:57:09 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,21 @@
+# Task 1: Perform Basic Mathematical Operations
+num1=float(input("Enter a num1:"))
+num2=float(input("Enter a num2:"))
+# Addition
+sum=num1+num2
+print("Addition :",sum)
+# subtraction
+sub=num1-num2
+print("subraction",sub)
+# Multiplication
+mul=num1*num2
+print("Multiplication:",mul)
+# division
+div=num1/num1
+print("Division",div)
+
+# Task 2: Create a Personalized Greeting
+first_name=str(input("Enter a first name :"))
+Last_name=str(input("Enter a last name:"))
+full_name=first_name+""+Last_name
+print("Hello,",full_name +"! welcome to python program")
