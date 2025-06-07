5107b66ad60ec506b201cbd7e8109b021d1944be
Bhushan-Sonune <sonunebhushan2@gmail.com>
2025-06-07 11:54:07 +0530

    Add files via upload

---
+++ simmple_python_task1.py
@@ -0,0 +1,14 @@
+num1=float(input("Enter a num1:"))
+num2=float(input("Enter a num2:"))
+# Addition
+sum=num1+num2
+print("Addition :",sum)
+# subtraction
+sub=num1+num2
+print("subraction",sub)
+# Multiplication
+mul=num1*num2
+print("Multiplication:",mul)
+# division
+div=num1/num1
+print("Division",div)
